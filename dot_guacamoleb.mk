9e0a15903c07494c4ea4880a312f031a8492eb8e
Codecity001 <sanganiharish263@gmail.com>
2021-05-02 18:13:27 +0530

    Vendor config adapted for DotOS

--- dot_guacamoleb.mk
+++ dot_guacamoleb.mk
@@ -12,7 +12,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/oneplus/guacamoleb/device.mk)
 
 # Inherit some common DotOS stuff.
-$(call inherit-product, vendor/dot/config/common_full_phone.mk)
+$(call inherit-product, vendor/dot/config/common.mk)
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := dot_guacamoleb
