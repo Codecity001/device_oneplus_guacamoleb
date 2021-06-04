7cb530a02d2b7a8d7665491a599317595cd65c71
Codecity001 <sanganiharish263@gmail.com>
2021-06-04 13:25:33 +0200

    Added:OPgallery,OPfilemanager,OPscreenrecorder

--- device.mk
+++ device.mk
@@ -8,6 +8,7 @@ $(call inherit-product, device/oneplus/sm8150-common/common.mk)
 
 # Get non-open-source specific aspects
 $(call inherit-product, vendor/oneplus/guacamoleb/guacamoleb-vendor.mk)
+$(call inherit-product, vendor/addons/oneplus/apps/config.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
