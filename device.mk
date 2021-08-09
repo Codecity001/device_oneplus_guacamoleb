9e797e6ef665c562383295f5a7e54c79354a42b1
Codecity001 <sanganiharish263@gmail.com>
2021-08-09 03:02:18 +0200

    Revert "guacamoleb: added OOS cam conditionally"
    This reverts commit 83bab5d682e700eb1ba18f5287bd726a96f01e6e.

--- device.mk
+++ device.mk
@@ -10,7 +10,6 @@ $(call inherit-product, device/oneplus/sm8150-common/common.mk)
 $(call inherit-product, vendor/oneplus/guacamoleb/guacamoleb-vendor.mk)
 $(call inherit-product-if-exists, vendor/addons/google/camera/config.mk)
 $(call inherit-product-if-exists, vendor/addons/oneplus/apps/config.mk)
-$(call inherit-product-if-exists, vendor/oneplus/camera/camera-vendor.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
